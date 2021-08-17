868c18f809417f636a34aa46f02c268a7cadca9c
Chotika-SE62 <59428867+Chotika-SE62@users.noreply.github.com>
2021-08-17 15:48:06 +0700

    Initial commit

--- src/components/Vaccine.js
+++ src/components/Vaccine.js
@@ -3,7 +3,26 @@ import './Vaccine.css';
 import VacImage from './VacImage';
 
 class Vaccine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // nameCompony = "Astraxeneca",
+      // nameInter: 'AZD1222',
+      // produce = "England",
+      // technology = "ไวรัส Adonvirus พาหะ",
+      short: '10000000',
+      have: true,
+    };
+  }
+//state = ดึงขึ้นมาจัดเก็บไว้ แล้วใช้งาน 
+    // component
+    //   this.setState({})
+
   render() {
+    if (!this.state.have) { //เงื่อไข,ค่าคงที่ในการแสดงผล
+      return <div>Not Found</div>;
+    }
+
     return (
       <div className="vaccine__list">
         <VacImage path="/img/1.jpg" />
@@ -11,8 +30,15 @@ class Vaccine extends React.Component {
         <div className="vaccine__item">{this.props.nameInter}</div>
         <div className="vaccine__item">{this.props.produce}</div>
         <div className="vaccine__item">{this.props.technology}</div>
+        <div>{this.state.short}</div> 
+        <div>{`${this.state.have}`}</div>
       </div>
     );
+    // } else {
+    //   return (
+    //     <div>Not found</div>
+    //   )
+    // }
   }
 }
 
